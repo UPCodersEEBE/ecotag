06756949a92c39a793c6a75afe4dfa2db8712d35
Joan Mas Secanell <joantarroja2000@gmail.com>
2021-05-15 02:57:15 +0200

    Afegir funció objecte

---
+++ process_typeform.py
@@ -0,0 +1,23 @@
+import uuid
+
+def get_info_from_quest (dict):
+    object_id = uuid.uuid4()
+    form_response=dict["form_response"]
+    answers=form_response["answers"]
+    
+    description = answers[0]["text"]
+    if answers[1]["boolean"]:
+        predecessors = answers[2]["text"]
+        predecessors = list(predecessors)
+    else:
+        predecessors = []
+
+    object_info = {
+        "object_id":object_id,
+        "description" :description,
+        "predecessors":predecessors
+    }
+
+    return object_info
+
+
