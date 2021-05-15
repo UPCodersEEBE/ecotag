4c99aaf3f7b58ed3fa231839ed4a344d71f27bb4
Joan Mas Secanell <joantarroja2000@gmail.com>
2021-05-15 10:36:44 +0200

    Update queries_mongo.py

--- queries_mongo.py
+++ queries_mongo.py
@@ -9,6 +9,8 @@ objects = db.objects
 def get_events_id_from_obj(object_id):
     object=objects.find_one({"_id": object_id})
     events = object["events"]
+    print(events)
+    print(type(events))
     if type(events)==None:
         events=[]
     return events
