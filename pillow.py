7e8f92945ab75cde8c3e287d31199fba4dbb5d44
Joan Mas Secanell <joantarroja2000@gmail.com>
2021-05-15 13:07:38 +0200

    remove coses

--- pillow.py
+++
@@ -1,8 +0,0 @@
-
-import segno
-qr = segno.make('The Beatles -- Let It Be', error='h')
-qr.to_artistic(background='yellowc.jpg', target='letitbe.jpg', scale=20)
-
-def normal_qr(id):
-    qr = segno.make(id, micro=False)
-    return qr
